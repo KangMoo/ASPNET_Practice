019744110e6180bbb7272260384e0bb4240863de
KangMoo <hkm0629@naver.com>
2020-08-31 22:24:35 +0900

    update

---
+++ Built_in_Object/HelloASP2/FrmResponseRedirect.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloASP2
+{
+    public partial class FrmResponseRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnDotNetKorea_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://www.dotnetkorea.com/");
+        }
+        protected void btnDevLec_Click(object sender, EventArgs e)
+        {
+            string strUrl = "http://www.devlec.com/";
+            Response.Redirect(strUrl);
+        }
+        protected void btnVisualAcademy_Click(object sender, ImageClickEventArgs e)
+        {
+            string strUrl = String.Format(
+                "http://{0}/{1}?{2}={3}", "www.visualAcademy.com","default.htm","UserID", "RedPlus");
+            Response.Redirect(strUrl);
+        }
+    }
+}
---
+++ Built_in_Object/HelloASP2/FrmResponseWrite.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmResponseWrite.aspx.cs" Inherits="HelloASP2.FrmResponseWrite" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Response 개체</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnClick" runat="server" Text="클릭" OnClick="btnClick_Click" />
+            <br />
+            <%="See you later<br />"%>
+            <asp:Button ID="btnJavaScript" runat="server"
+                TEXT="Javascript Hello" OnClick="btnJavaScript_Click" />
+        </div>
+    </form>
+</body>
+</html>
